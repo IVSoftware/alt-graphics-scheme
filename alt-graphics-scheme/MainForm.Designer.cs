dac2be0ada8817d9f1fee71132c3eb502403e209
IVSoftware <tgregor@ivsoftware.com>
2022-12-31 10:50:54 -0700

    Add project files.

---
+++ alt-graphics-scheme/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace alt_graphics_scheme
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxMyBool = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBoxMyBool
+            // 
+            this.checkBoxMyBool.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBoxMyBool.AutoSize = true;
+            this.checkBoxMyBool.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxMyBool.Name = "checkBoxMyBool";
+            this.checkBoxMyBool.Size = new System.Drawing.Size(52, 23);
+            this.checkBoxMyBool.TabIndex = 0;
+            this.checkBoxMyBool.Text = "MyBool";
+            this.checkBoxMyBool.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBoxMyBool);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxMyBool;
+    }
+}
+
---
+++ alt-graphics-scheme/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alt_graphics_scheme
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var dir = 
+                Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "Backgrounds");
+
+            _backgrounds =
+                Directory
+                .GetFiles(dir)
+                .Select(_ => (Bitmap)Bitmap.FromFile(_))
+                .ToArray();
+            BackgroundImage = _backgrounds[0];
+            checkBoxMyBool.CheckedChanged += (sender, e) => MyBool = checkBoxMyBool.Checked;
+        }
+        bool _myBool = false;
+        public bool MyBool
+        {
+            get => _myBool;
+            set
+            {
+                if (!Equals(_myBool, value))
+                {
+                    _myBool = value;
+                    _ = onMyBoolChanged();
+                }
+            }
+        }
+        private readonly Bitmap[] _backgrounds;
+
+        private async Task onMyBoolChanged()
+        {
+            if(MyBool)
+            {
+                for (int i = 1; i < _backgrounds.Length; i++)
+                {
+                    var captureImage = _backgrounds[i];
+                    BeginInvoke((MethodInvoker)delegate 
+                    {
+                        BackgroundImage = captureImage;
+                    });
+                    await Task.Delay(100);
+                }
+            }
+            else
+            {
+                BackgroundImage = _backgrounds[0];
+            }
+        }
+    }
+}
